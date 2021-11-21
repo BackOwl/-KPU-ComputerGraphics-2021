5c4c2d18f2acace863e385a1f62d0d973a923936
Leesiw <84753357+Leesiw@users.noreply.github.com>
2021-11-21 18:10:26 +0900

    프래그먼트 쉐이더 수정

--- ComputerGraphicsFinal/fragment.glsl
+++ ComputerGraphicsFinal/fragment.glsl
@@ -16,7 +16,7 @@ void main()
 
 	float ambientLight = ambientlight; //--- 주변 조명 세기
 	vec3 ambient = ambientLight * lightColor; //--- 주변 조명 값
-	if (Shininess != 0.0) {
+
 		vec3 normalVector = normalize (Normal);
 		vec3 lightDir = normalize (lightPos - FragPos); //--- 표면과 조명의 위치로 조명의 방향을 결정한다.
 		float diffuseLight = max (dot (normalVector, lightDir), 0.0); //--- N과 L의 내적 값으로 강도 조절: 음수 방지
@@ -30,12 +30,5 @@ void main()
 		vec3 specular = specularLight * lightColor; //--- 거울 반사 조명값: 거울반사값 * 조명색상값
 		vec3 result;
 		result = (ambient + diffuse + specular) * outColor; //--- 최종 조명 설정된 픽셀 색상: (주변+산란반사+거울반사조명)*객체 색상
-		//result = (ambient + specular) * outColor;
 		FragColor = vec4 (result, 1.0); // --- 픽셀 색을 출력
-	}
-	else{
-		vec3 result = ambient * outColor;
-		FragColor = vec4 (result, 1.0); // --- 픽셀 색을 출력
-	}
-	
 }
